d6329859f467e99e7727b17f592e4a4ffb496f70
Ubuntu <ubuntu@instance-1.asia-southeast1-b.c.boreal-doodad-265509.internal>
2020-07-13 18:40:05 +0000

    cwcqe

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from './app.js';
 import config from './config/index.js';
 
-const { PORT } = config;
-
+const PORT = process.env.PORT || 9999;
+console.log("Port", PORT)
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
